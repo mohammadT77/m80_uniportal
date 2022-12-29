6971670535cae536bf4804c42975280acaf59395
matehrani <t.mohammadamin@gmail.com>
2022-12-29 09:35:46 +0100

    feat: enable i18n, feat: add name to models

--- education/models.py
+++ education/models.py
@@ -7,10 +7,18 @@ from core.models import BaseModel, User
 
 
 class Department(BaseModel):
-    pass
+    class Meta:
+        verbose_name = _("Department")
+        verbose_name_plural = _("Departments")
+
+    name = models.CharField(_("Department name"), max_length=20, null=False)
 
 
 class Student(User):
+    class Meta:
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
+
     age = models.IntegerField(_("Age"))
     dept = models.ForeignKey(Department, on_delete=models.RESTRICT, verbose_name=_("Department"), null=False)
     degree = models.ForeignKey("Degree", on_delete=models.RESTRICT, verbose_name=_("Degree"), null=False)
@@ -18,10 +26,18 @@ class Student(User):
 
 
 class Professor(User):
+    class Meta:
+        verbose_name = _("Professor")
+        verbose_name_plural = _("Professors")
+
     dept = models.ForeignKey(Department, on_delete=models.RESTRICT, verbose_name=_("Department"))
 
 
 class Course(BaseModel):
+    class Meta:
+        verbose_name = _("Course")
+        verbose_name_plural = _("Courses")
+
     exam_date = models.DateTimeField(_("Exam date"))
     name = models.CharField(_("Course name"), max_length=20)
 
@@ -32,11 +48,19 @@ class Course(BaseModel):
 
 
 class Semester(BaseModel):
+    class Meta:
+        verbose_name = _("Semester")
+        verbose_name_plural = _("Semesters")
+
     start_date = models.DateField(_("Start date"))
     end_date = models.DateField(_("End date"))
 
 
 class Degree(BaseModel):
+    class Meta:
+        verbose_name = _("Degree")
+        verbose_name_plural = _("Degrees")
+
     level = models.CharField(_("Level"), choices=[
         ("B", _("Bachelor")),
         ("M", _("Master")),
