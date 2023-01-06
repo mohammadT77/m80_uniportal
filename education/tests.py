1978619c9f80c4f7ca4af747d55a42b1de3af4af
matehrani <t.mohammadamin@gmail.com>
2023-01-06 09:31:20 +0100

    feat: impl api test :)

--- education/tests.py
+++ education/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 # Create your tests here.
+class SemesterViewTest(TestCase):
+    def test1_get_semesters_list(self):
+        client = APIClient(enforce_csrf_checks=False)
+        client.login(username='student1', password='AkbarRezaii1234')
+
+        resp = client.get('/api/education/semester/')
+        print(dir(resp))
+        print(resp.data)
+        print(resp.content)
